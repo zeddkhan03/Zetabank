b6d369fa34d38abac385a88fe2cc0ce592940da0
Mohd Zaid Khan <111868309+zeddkhan03@users.noreply.github.com>
2023-02-15 01:09:29 +0530

    Zetabank on ICP
    A De-fi deployed on ICP blockchain with the help of Canisters checked through Candid UI.

---
+++ src/dbank_assets/src/index.js
@@ -0,0 +1,51 @@
+import { UpdateCallRejectedError } from "@dfinity/agent";
+import { dbank } from "../../declarations/dbank";
+
+window.addEventListener("load", async function() {
+  //console.log("Finished loading");
+  update();
+  //UpdateCallRejectedError();
+  /*const currentAmount = await dbank.checkBalance();
+  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;*/
+});
+
+document.querySelector("form").addEventListener("submit", async function(event) {
+  event.preventDefault();
+  //console.log("Submitted.");
+
+  const button = event.target.querySelector("#submit-btn");
+
+  const inputAmount = parseFloat(document.getElementById("input-amount").value);
+  const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+
+  button.setAttribute("disabled", true);
+
+  if (document.getElementById("input-amout").value.length != 0) {
+    await dbank.topUp(inputAmount);
+  }
+
+  if (document.getElementById("withdrawal-amount").value.length != 0) {
+    await dbank.withdrawal(outputAmount);
+  }
+
+  await dbank.compound();
+
+  /*await dbank.topUp(inputAmount);
+  await dbank.withdrawl(outputAmount);*/
+
+  update();
+
+  document.getElementById("input-amount").value = "";
+  document.getElementById("withdrawal-amount").value = "";
+
+  button.removeAttribute("disabled");
+
+  });
+
+  async function update() {
+  const currentAmount = await dbank.checkBalance();
+  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+
+  };
+
+
